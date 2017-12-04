a908d8e0aacacfa6386f276bb9b47732719f635e
Zander Work <zander@zanderwork.com>
2017-12-04 11:48:11 -0800

    fixed a bug where having multiple characters on a server would allow duplicate ladders to be created w/o differentiation other than mmr, league, and division.

--- starcraft/player.py
+++ starcraft/player.py
@@ -13,4 +13,16 @@ class Player:
         self.ladders.append(team)
 
     def add_ladder(self, ladder: Team):
-        self.ladders.append(ladder)
+        race_already_exists = False
+        for team in self.ladders:
+            if ladder.race == team.race:
+                # if a player has multiple characters on a server, it's possible to have multiple 1v1 ladders for the
+                # same race. if so, we want to keep the highest mmr one
+                race_already_exists = True
+                if ladder.mmr > team.mmr:
+                    team.mmr = ladder.mmr
+                    team.league = ladder.league
+                    team.divison = ladder.divison
+                    team.games_played = ladder.games_played
+        if not race_already_exists:
+            self.ladders.append(ladder)
