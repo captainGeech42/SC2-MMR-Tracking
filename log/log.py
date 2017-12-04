f42ee7c9e3dc901c421c32123119df7537df6230
Zander Work <zander@zanderwork.com>
2017-12-03 19:17:42 -0800

    made the log class static

--- log/log.py
+++ log/log.py
@@ -3,26 +3,26 @@ import os
 
 
 class Log:
-    def __init__(self, filepath: str = None):
-        self.filepath = filepath
+    filepath = None
 
     @staticmethod
-    def get_timestamp()->str:
+    def get_timestamp() -> str:
         return time.strftime("%H:%M:%S")
 
-    def write_log_message(self, message: str, error: bool = False):
+    @staticmethod
+    def write_log_message(message: str, error: bool = False):
         log_type = "ERROR" if error else "INFO"
-        msg = "{} {}: {}".format(self.get_timestamp(), log_type, message)
+        msg = "{} {}: {}".format(Log.get_timestamp(), log_type, message)
         print(msg)
 
-        if self.filepath is not None:
+        if Log.filepath is not None:
             try:
-                log_file = open(self.filepath, "a")
+                log_file = open(Log.filepath, "a")
                 log_file.write(msg + os.linesep)
                 log_file.close()
             except OSError:
-                fp = self.filepath
-                self.filepath = None
-                self.write_log_message(
+                fp = Log.filepath
+                Log.filepath = None
+                Log.write_log_message(
                     "Could not write to log file '{}' (no more attempts will be made to write to this file)".format(fp),
                     True)
